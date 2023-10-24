6df00ccd9d25d3f00873f7641c3e96ee2c948e11
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2023-10-24 15:02:08 -0400

    Skip double null chacks in shared arrays

--- src/runtime/shared-array.c
+++ src/runtime/shared-array.c
@@ -4,7 +4,7 @@
 #define AG_RELEASE(PTR) ag_release_shared((AgObject*)(PTR))
 #define AG_RETAIN(PTR) ag_retain_shared((AgObject*)(PTR))
 #define AG_RETAIN_OWN(PTR, PARENT) ag_retain_shared((AgObject*)(PTR))
-#define AG_RETAIN_OWN_NN(PTR, PARENT) ag_retain_shared((AgObject*)(PTR))
+#define AG_RETAIN_OWN_NN(PTR, PARENT) ag_retain_shared_nn((AgObject*)(PTR))
 #define AG_COPY(TO, FROM, PARENT) { *TO = *FROM; ag_retain_shared(*(AgObject**)(FROM)); }
 #define AG_VISIT_KIND AG_VISIT_OWN
 #define AG_ITEM_TYPE AgObject*
