24c93c7d0c28b0518e938f542873b192b6c2ef5b
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2024-06-19 20:10:48 -0400

    Support win64 standalone linker and msvc

--- output/bin/run-release.bat
+++ output/bin/run-release.bat
@@ -1,7 +1,12 @@
 @pushd "%~dp0..\workdir"
+@if exist "%~dp0lld-link.exe" (
+   set linker="%~dp0lld-link.exe" /libpath:"..\libs" /out:
+) else (
+   set linker="cl.exe" /Fe
+)
 @if exist "..\apps\%~1.obj" del "..\apps\%~1.obj"
 @"%~dp0agc" -src "..\ag" -start %1 -O3 -o "..\apps\%~1.obj"
-@IF '%ERRORLEVEL%'=='0' "%~dp0lld-link" /out:"..\apps\%~1.exe" /libpath:"..\libs" "..\apps\%~1.obj" "..\libs\ag_runtime.lib" ^
+@IF '%ERRORLEVEL%'=='0' %linker%"..\apps\%~1.exe" "..\apps\%~1.obj" "..\libs\ag_runtime.lib" ^
    "..\libs\ag_sdl.lib" "..\libs\SDL2.lib" "..\libs\SDL2_image.lib" "..\libs\SDL2_ttf.lib" ^
    "..\libs\ag_http_client.lib" "..\libs\libcurl.lib" ^
    "..\libs\ag_sqlite.lib" "..\libs\sqlite3.lib"
