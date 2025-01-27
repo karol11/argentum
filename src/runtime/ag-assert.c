5594feb6dd5ff084b7b234e1a69bc54bda197365
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-01-26 22:29:22 -0500

    Move tests to ag and all test-related ffi functions to tests module

--- src/runtime/ag-assert.c
+++ src/runtime/ag-assert.c
@@ -1,7 +1,7 @@
 #include <stdio.h> // int32_t int64_t
 #include "runtime.h"
 
-void ag_fn_sys_assert0(bool v) {
+void ag_fn_tests_assert(bool v) {
 	if (!v) {
 		fputs("assert failure\n", stdout);
 		exit(-1);
@@ -10,8 +10,12 @@ void ag_fn_sys_assert0(bool v) {
 
 static int64_t foreign_test_function_state = 0;
 
-int64_t foreign_test_function(int64_t delta) {
+int64_t ag_fn_tests_ffiTestFn(int64_t delta) {
 	return foreign_test_function_state += delta;
+}
+
+void ag_fn_tests_ffiTestReset() {
+    foreign_test_function_state = 0;
 }
 
 void void_void_trampoline(AgObject* self, ag_fn entry_point, ag_thread* th) {
@@ -21,8 +25,8 @@ void void_void_trampoline(AgObject* self, ag_fn entry_point, ag_thread* th) {
         ((void (*)(AgObject*)) entry_point)(self);
     // release params here, if any
 }
-void ag_fn_sys_asyncFfiCallbackInvoker(AgWeak* cb_data, ag_fn cb_entry_point) {
+void ag_fn_tests_asyncFfiCallbackInvoker(AgWeak* cb_data, ag_fn cb_entry_point) {
     ag_retain_weak(cb_data);
     ag_thread* th = ag_prepare_post_from_ag(cb_data, cb_entry_point, void_void_trampoline, 0);
-    // post params here
+    // post params here if any
 }
